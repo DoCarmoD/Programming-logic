c158866a99eef0f54df4299ab563ce2b2e09c577
DoCarmo <98768795+DoCarmoD@users.noreply.github.com>
2022-12-28 20:19:13 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,4 +1,6 @@
 #Média Aritimética do Aluno
+# Student Arithmetic Mean
+
 nota1 = float(input("Qual nota o aluno tirou no primeiro bimestre? "))
 nota2 = float(input("Qual nota o aluno tirou no segundo bimestre? "))
 media = (nota1 + nota2) / 2
