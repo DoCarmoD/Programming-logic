11ff68f96c4f7778472c0ea3d944c0360d5d2326
DoCarmo <98768795+DoCarmoD@users.noreply.github.com>
2022-12-28 20:16:52 -0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -1,4 +1,5 @@
 # Pergunte o nome do usuário, e responda com "Prazer em te conhecer + nome !"
+# Ask for the username, and reply with "Nice to meet you + name !"
 
 nome = input('Qual é o seu nome?')
 print(f'Olá {nome} , muito prazer em te conhecer!')
