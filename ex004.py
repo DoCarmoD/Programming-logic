1eb6ff64a1f3ed4457a3932f6fa465615de0b89d
DoCarmo <98768795+DoCarmoD@users.noreply.github.com>
2022-12-28 20:18:26 -0300

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -1,4 +1,5 @@
 #Pergunte ao usuário um número e mostre na tela seu antecessor e sucessor 
+#Ask the user a number and show on the screen its predecessor and successor
 
 n = int(input('Digite um valor: '))
 an = n - 1
